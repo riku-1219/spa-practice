b59d880a152246c293a6cb238a238880f314136c
rikuto ui <mdr.crew.1219@gmail.com>
2020-12-14 04:18:18 +0900

    initial commit

---
+++ src/CheckBox.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+
+const CheckBox = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [prefs, setPrefs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      headers: {
+        "X-API-KEY": 'txoJHsTPn2FZJH94ezJGJ9OxKejn76ecrgOrwDlb'
+      }
+    })
+    .then(res => res.json())
+    .then(
+      (data) => {
+        setIsLoaded(true);
+        setPrefs(data.result);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+}, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        {prefs.map(item => (
+          <>
+          <label htmlFor="check">{item.prefName}：</label>
+          <input type="checkbox" id="check" />
+          </>
+        ))}
+      </> 
+    );
+  }   
+}
+
+export default CheckBox;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CheckBox from './CheckBox'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CheckBox />
   </React.StrictMode>,
   document.getElementById('root')
 );
